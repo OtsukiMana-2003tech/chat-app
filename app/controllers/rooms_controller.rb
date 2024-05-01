7fdca4072f54fb37e0cc99aaeea9252a0ccc3050
OtsukiMana-2003tech <rstoyo3.otsuki.mana@techcamp-training.com>
2024-05-01 17:20:05 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,20 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    # 保存に成功した場合と失敗した場合の実行を分けておく
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
